216480dea47c152989d9f6279ceb12e3a59a5893
Ayden Xu <aydenxunc@gmail.com>
2025-05-09 22:13:23 -0400

    pixel

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@
 import type { Metadata } from "next";
 import Script from "next/script";
 
+const PIXEL_ID = "9e96b458-a285-490d-9d76-eb701b8c9078";
+
 export const metadata: Metadata = {
   title: "Earn Gift Cards with Swagbucks",
   description: "Start earning rewards today.",
@@ -11,41 +13,32 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en">
       <head>
-        {/* Google Tag Manager */}
-        <Script id="google-tag-manager" strategy="afterInteractive">
-          {`
-            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-NBCBRTPG');
-          `}
-        </Script>
+        {/* …other tags… */}
 
-        {/* Snap Pixel Base */}
-        <Script id="snap-pixel" strategy="afterInteractive">
+        {/* 1) Load Snap Pixel library */}
+        <Script
+          src="https://sc-static.net/scevent.min.js"
+          strategy="afterInteractive"
+          id="snap-pixel-lib"
+        />
+
+        {/* 2) Init your Pixel + fire PAGE_VIEW */}
+        <Script id="snap-pixel-init" strategy="afterInteractive">
           {`
-            (function(e,t,n){if(e.snaptr)return;var a=e.snaptr=function(){
-              a.handleRequest?a.handleRequest.apply(a,arguments):a.queue.push(arguments)};
-              a.queue=[];var s='script';r=t.createElement(s);r.async=!0;
-              r.src=n;var u=t.getElementsByTagName(s)[0];
-              u.parentNode.insertBefore(r,u);})(window,document,
-              'https://sc-static.net/scevent.min.js');
-              snaptr('init', '9e96b458-a285-490d-9d76-eb701b8c9078');
-              snaptr('track', 'PAGE_VIEW');
+            window.snaptr = window.snaptr || function() {
+              (window.snaptr.q = window.snaptr.q || []).push(arguments)
+            };
+            snaptr('init', '${PIXEL_ID}', {
+              // If you have the user’s email at build time, interpolate here.
+              // Otherwise omit or pass empty string.
+              'user_email': '' 
+            });
+            snaptr('track', 'PAGE_VIEW');
           `}
         </Script>
       </head>
       <body>
-        {/* GTM (noscript) */}
-        <noscript>
-          <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-NBCBRTPG"
-            height="0"
-            width="0"
-            style={{ display: "none", visibility: "hidden" }}
-          ></iframe>
-        </noscript>
+        {/* … */}
         {children}
       </body>
     </html>
